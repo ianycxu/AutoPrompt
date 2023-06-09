77d289726addc8832b8852fc2152da756a64fdb3
Yinchuan Xu <yinchuanxu@microsoft.com>
2023-06-08 17:40:59 -0700

    Add random_balanced_separate mode

--- data/reasoning_results/module/generate_summary_prompt.py
+++ data/reasoning_results/module/generate_summary_prompt.py
@@ -8,22 +8,38 @@ def get_representative(reasoning_results, sample_number = 5, sample_method = "ra
     """
     if sample_method == "random":
         return reasoning_results.sample(n = sample_number)
-    elif sample_method == "random_balanced":
+    elif sample_method == "random_balanced" or sample_method == "seg_df":
         return reasoning_results.groupby('output', group_keys=False).apply(lambda x: x.sample(sample_number))
     else:
         # TODO: clustering based
         return reasoning_results
 
 
-def form_prompt(template, reasoning_results, description):
+def form_prompt(template, reasoning_results, description, sample_method):
     
     template = template.replace("[DESC]", description)
     
     data = "\n"
-    for index, row in reasoning_results.iterrows():
-        data += "Input: "  + row["input"] + "\n"
-        data += "Output: "  + row["output"] + "\n"
-        data += "Reason: "  + row["answer"].replace(":\\n\\n","") + "\n"
+    if sample_method == "random_balanced_separate":
+        for label in reasoning_results['output'].value_counts().index:
+            seg_df = reasoning_results[reasoning_results['output'] == label]
+            seg_data = f"Cases for category: {label} \n"
+            i = 1
+            for index, row in seg_df.iterrows():
+                seg_data += f"Data point {i}" + "\n"
+                seg_data += "Input: "  + row["input"] + "\n"
+                seg_data += "Output: "  + row["output"] + "\n"
+                seg_data += "Reason: "  + row["answer"].replace(":\\n\\n","") + "\n\n"
+                i += 1
+            data += seg_data
+    else:
+        i = 1
+        for index, row in reasoning_results.iterrows():
+            data += f"Data point {i}" + "\n"
+            data += "Input: "  + row["input"] + "\n"
+            data += "Output: "  + row["output"] + "\n"
+            data += "Reason: "  + row["answer"].replace(":\\n\\n","") + "\n\n"
+            i += 1
 
 
     final_prompt = template.replace("[DATA]", data)
@@ -40,9 +56,9 @@ def main(reasoning_results, template, description, output_path, sample_number, s
     with open(description) as fin:
         description = fin.readline().strip() 
 
-    reasoning_results = get_representative(reasoning_results, sample_number = sample_number, sample_method=sample_method)
+    reasoning_results = get_representative(reasoning_results, sample_number, sample_method)
 
-    final_prompt = form_prompt(template, reasoning_results, description)
+    final_prompt = form_prompt(template, reasoning_results, description, sample_method)
 
     with open(output_path, 'w') as fout:
         fout.write(final_prompt)
@@ -62,7 +78,7 @@ if __name__ == '__main__':
         input3_path = '../epistemic_reasoning/description.tsv'
         output_path = '../epistemic_reasoning/final prompt.tsv'
         sample_number = 2
-        sample_method = "random_balanced"
+        sample_method = "random_balanced_separate"
 
     main(input1_path, input2_path, input3_path,
          output_path, sample_number, sample_method)
